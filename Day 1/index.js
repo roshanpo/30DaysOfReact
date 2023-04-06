e7a7fe1b1c564d3612a9c30710ed85e1e5724167
Roshan <roshanpokhrel013@gmail.com>
2023-04-07 01:51:00 +0545

    My first Code of React

---
+++ Day 1/index.js
@@ -0,0 +1,13 @@
+//ReactDOM.render(<h1>Hello! I'm starting 30 days of React!</h1>, document.getElementById("root"));
+
+const Navbar = (
+    <nav>
+        <h1> Roshay </h1>
+        <ul>
+            <li> Pricing </li>
+            <li> About</li>
+            <li> Contact</li>
+        </ul>
+    </nav>   
+)
+ReactDOM.render(Navbar, document.getElementById("root"));

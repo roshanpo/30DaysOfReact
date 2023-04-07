ade39ea57e6dc80d0ef5c1067ba9d3902a91fba2
Roshan <roshanpokhrel013@gmail.com>
2023-04-08 01:05:57 +0545

    First project on nextjs

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function(){
+    return <>
+    <h1>My first post!</h1>
+
+    <h2>
+        <Link href='/'>Back to Home</Link>
+    </h2>
+    </>
+}

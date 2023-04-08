02f18ccd0127b08bdd81ef973c2f7aa8108a706e
Roshan <roshanpokhrel013@gmail.com>
2023-04-08 23:02:24 +0545

    Played around with Nextjs

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,11 +1,28 @@
 import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../styles/components/layout";
+
 
 export default function(){
     return <>
+    <Layout>
+    <Head>
+        <title>First Post</title>
+        {/* <
+            Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnLoad"
+            onLoad={() =>
+                console.log(`script loaded correctly, window.FB has been populated`)
+              }
+            /> */}
+    </Head>
     <h1>My first post!</h1>
 
     <h2>
         <Link href='/'>Back to Home</Link>
     </h2>
+    </Layout>
     </>
 }

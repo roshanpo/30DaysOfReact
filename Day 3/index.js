a05d6c89d0566e8389fd2448321e79ef6d7d4912
Roshan <roshanpokhrel013@gmail.com>
2023-04-09 00:58:28 +0545

    Components
    Learned about different components and linking components together to make a app.

---
+++ Day 3/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
+
+/**
+Challenge: move the Footer and MainContent components
+into their own files.
+*/
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))

6df93f0699009906b441bcda50288652df46ce5b
Roshan <roshanpokhrel013@gmail.com>
2023-04-24 20:43:40 +0545

    Update App.js

--- Day 13/App.js
+++ Day 13/App.js
@@ -5,19 +5,24 @@ import Box from "./Box"
 export default function App() {
     const [squares, setSquares] = React.useState(boxes)
     
+    function toggle(id) {
+        setSquares(prevSquares => {
+            return prevSquares.map((square) =>{
+                    return square.id===id ? {...square, on:!square.on}: square
+                    
+        })}
+    
+        )
+    }
+    
     const squareElements = squares.map(square => (
-        <Box key={square.id} on={square.on} />
+        <Box 
+            key={square.id} 
+            id={square.id}
+            on={square.on} 
+            toggle={toggle}
+        />
     ))
-    /**
-     * Challenge part 2:
-     * 1. Create a separate component called "Box" and
-     *    replace the `div` above with our <Box /> components
-     * 2. Pass the Box component a prop called `on` with the
-     *    value of the same name from the `boxes` objects
-     * 3. In the Box component, apply dynamic styles to determine
-     *    the backgroundColor of the box. If it's `on`, set the
-     *    backgroundColor to "#222222". If off, set it to "none"
-     */
     
     return (
         <main>

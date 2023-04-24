1c04289bbe7d09e56ce377a36541faa3416c0ba6
Roshan <roshanpokhrel013@gmail.com>
2023-04-24 20:46:02 +0545

    Update Box.js

--- Day 13/Box.js
+++ Day 13/Box.js
@@ -1,21 +1,16 @@
 import React from "react"
 
-
 export default function Box(props) {
-   
-    const [color, setcolor] = React.useState(props.on)
-    
-    function changeColor(){
-        setcolor(prevColor => !prevColor)
-        
-    }
-    
-    
     const styles = {
-        backgroundColor: color ? "#222222" : "transparent"
+        backgroundColor: props.on ? "#222222" : "transparent"
     }
     
     return (
-        <div style={styles} className="box" onClick={changeColor}></div>
+        <div 
+            style={styles} 
+            className="box"
+            onClick={()=>props.toggle(props.id)}
+        >
+        </div>
     )
 }

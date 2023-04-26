d44883b90c7c6edcf3a32bc2db5a1dfdcdd94d2e
Roshan <roshanpokhrel013@gmail.com>
2023-04-26 19:33:25 +0545

    Update Form.js

--- Day 15/Form.js
+++ Day 15/Form.js
@@ -2,14 +2,8 @@ import React from "react"
 
 export default function Form() {
     const [formData, setFormData] = React.useState(
-        {firstName: "", lastName: "", email: ""}
+        {firstName: "", lastName: "", email: "", comment:""}
     )
-    
-    /**
-     * Challenge: add an email field/state to the form
-     */
-    
-    console.log(formData)
     
     function handleChange(event) {
         setFormData(prevFormData => {
@@ -19,6 +13,7 @@ export default function Form() {
             }
         })
     }
+    console.log(formData)
     
     return (
         <form>
@@ -27,18 +22,28 @@ export default function Form() {
                 placeholder="First Name"
                 onChange={handleChange}
                 name="firstName"
+                value={formData.firstName}
             />
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange={handleChange}
                 name="lastName"
+                value={formData.lastName}
             />
             <input
                 type="email"
                 placeholder="Email"
                 onChange={handleChange}
                 name="email"
+                value={formData.email}
+            />
+            <textarea 
+                type="text"
+                placeholder="Comment"
+                name="comment"
+                onChange={handleChange}
+                value={formData.comment}
             />
         </form>
     )
